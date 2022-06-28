636e82e27d683a4fb79cd1c8aaf5e6f59efff51d
jrgirotto <jrgirotto@live.com>
2022-06-27 21:20:50 -0300

    updated to talk about class members

--- Kotlin/api-collections/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ Kotlin/api-collections/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -5,18 +5,20 @@ class Pessoa {
     var cpf: String = "123.123.123-90"
     private set
 
+    constructor()
+
+    fun pessoaInfo() = "$nome + $cpf"
+
 //    inner class Endereco {
 //        var rua: String = "Rua dos Códigos"
 //    }
-
 }
-
 fun main() {
     val junior = Pessoa()
 
     println(junior)
     println(junior.nome)
-    println(junior.cpf)
+    println(junior.pessoaInfo())
 
 //    println(junior.Endereco().rua)
 }
