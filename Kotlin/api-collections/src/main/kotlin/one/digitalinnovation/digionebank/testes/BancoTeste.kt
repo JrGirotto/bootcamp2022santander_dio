e476ff57deeae696abb01beeea6653eaafbc6101
jrgirotto <jrgirotto@live.com>
2022-06-27 21:53:17 -0300

    created data class and tests

---
+++ Kotlin/api-collections/src/main/kotlin/one/digitalinnovation/digionebank/testes/BancoTeste.kt
@@ -0,0 +1,15 @@
+package one.digitalinnovation.digionebank.testes
+
+import one.digitalinnovation.digionebank.Banco
+
+fun main() {
+    val digiOneBank = Banco(nome = "DigiOne", numero = 2)
+
+    println(digiOneBank.nome)
+    println(digiOneBank.numero)
+
+    val banco2 = digiOneBank.copy(nome = "Nubank")
+    //println(banco2.nome)
+    //println(banco2.numero)
+    println(banco2.info())
+}
