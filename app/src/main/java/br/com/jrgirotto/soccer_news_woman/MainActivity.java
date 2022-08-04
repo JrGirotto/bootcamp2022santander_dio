f79e46b9d9688b76072c529c9018bc5a16401779
jrgirotto <jrgirotto@live.com>
2022-08-04 18:29:48 -0300

    Desafio App Soccer News Woman

---
+++ app/src/main/java/br/com/jrgirotto/soccer_news_woman/MainActivity.java
@@ -0,0 +1,30 @@
+package br.com.jrgirotto.soccer_news_woman;
+
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import br.com.jrgirotto.soccer_news_woman.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        br.com.jrgirotto.soccer_news_woman.databinding.ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard).build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+
+}
